06139bd84a05910a6dee09b520b88a9e8bf31b5a
alumno1 <alumno@macbook04.local>
2017-03-09 20:46:59 -0500

    gesture 4

--- Gesture in iOS/ViewController.swift
+++ Gesture in iOS/ViewController.swift
@@ -23,21 +23,61 @@ class ViewController: UIViewController {
             let TapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tap))
         TapGestureRecognizer.numberOfTapsRequired = 2
             vwdemo.addGestureRecognizer(TapGestureRecognizer)
+        case "UIPinchGestureRecognizer":
+            let PinchGestureRecognizer =
+            UIPinchGestureRecognizer(target: self, action: #selector(pinch))
+            
+            PinchGestureRecognizer.scale = 2
+            
+         vwdemo.addGestureRecognizer(PinchGestureRecognizer)
+            
+        case "UIRotationGestureRecognizer" :
+            
+            let RotationGestureRecognizer = UIRotationGestureRecognizer(target: self, action: #selector(rotation))
+            
+            vwdemo.addGestureRecognizer(RotationGestureRecognizer)
+        
+        case "UISwipeGestureRecognizer" :
+            
+            let SwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(swipe))
+        
+            
+        SwipeGestureRecognizer.direction = UISwipeGestureRecognizerDirection.right
+            SwipeGestureRecognizer.numberOfTouchesRequired = 1
+            vwdemo.addGestureRecognizer(SwipeGestureRecognizer)
+            
             
         default:
         print("defalut")
         }
         
-        
-        //UIPinchGestureRecognizer
-        //UIRotationGestureRecognizer
-        //UISwipeGestureRecognizer
+    
+        //
+        //
         //UIPanGestureRecognizer
         //UIScreenEdgePanGestureRecognizer
         //UILongPressGestureRecognizer
      
         print(tipo)
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    func swipe(){
+        print("swipe")
+    }
+    
+    func rotation(sender : UIRotationGestureRecognizer){
+        
+        print("Rotation")
+        print("Rotation : \(sender.rotation)")
+        print("velocidad: \(sender.velocity)")
+    }
+    
+    func pinch(sender:UIPinchGestureRecognizer){
+        print("pinch")
+        print("Scale: \(sender.scale)")
+        print("velocidad: \(sender.velocity)")
     }
 
     func tap(){
