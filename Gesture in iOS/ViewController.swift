b1fb211f3351cd6d197fed71e4946e9740b221ce
alumno1 <alumno@macbook04.local>
2017-03-09 20:17:09 -0500

    base

--- Gesture in iOS/ViewController.swift
+++ Gesture in iOS/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var tipo:String!
+    
+    @IBOutlet weak var vwdemo: UIView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        switch tipo {
+        case "UITapGestureRecognizer":
+            
+            let TapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tap))
+        TapGestureRecognizer.numberOfTapsRequired = 2
+            vwdemo.addGestureRecognizer(TapGestureRecognizer)
+            
+        default:
+        print("defalut")
+        }
+        
+        
+        //UIPinchGestureRecognizer
+        //UIRotationGestureRecognizer
+        //UISwipeGestureRecognizer
+        //UIPanGestureRecognizer
+        //UIScreenEdgePanGestureRecognizer
+        //UILongPressGestureRecognizer
+     
+        print(tipo)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    func tap(){
+        print("Tap")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
